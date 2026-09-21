52b3135da7c549acde34be3af3b6d486166f8963
manel <toutouhelwa@gmail.com>
2026-09-21 13:37:52 +0200

    refactor(App): update greeting logic and replace profile image with avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, Container } from "react-bootstrap";
-
 import Name from "./components/Name";
 import Price from "./components/Price";
 import Description from "./components/Description";
@@ -8,16 +7,14 @@ import Image from "./components/Image";
 import Navigation from "./components/Navigation";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Footer from "./components/Footer";
-import profileImage from "./assets/profileImage.jpg"
-<source />
-
-const firstName = "Manel";
-
+import imgAvatar from './assets/avatar.png'
 function App() {
+  const firstName = "";
   return (
     <>
       <Navigation />
+      <h1 className="text-center py-2">Hello,{firstName ? firstName : "there"}</h1>
+      {firstName &&<img src={imgAvatar} style={{ width: "100px" }} className="d-block m-auto" />}
       <Container className="text-center p-3">
         <Row className="g-4 justify-content-center">
 
@@ -113,24 +110,8 @@ function App() {
             </Card>
           </Col>
         </Row>
-        {firstName && (
-      <img
-          src={profileImage}
-        style={{
-          width: "22rem",
-          borderRadius: "20px",
-          textAlign:"center"
-        }}
-        alt="Profile"
-        className="img-fluid mt-3"
-      />
-      )}
-      <h3 className="mt-3">
-      {firstName ? `Hello, ${firstName}!` : "Hello, there!"}
-    </h3>
       </Container>
-      
-    <Footer></Footer>
+   
 
     </>
   );
